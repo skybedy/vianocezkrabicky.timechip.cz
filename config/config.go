3a8d23991e091dad4d0e6e86176b59a53ece86e1
skybedy <skybedy@gmail.com>
2021-01-13 08:46:44 +0100

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,14 +1,14 @@
 package confx
 
 const DbDriver = "mysql"
-const DbUser = "skybedy"
-const DbPass = "mk1313life"
-const DbName = "timechip_cz"
+const DbUser = ""
+const DbPass = ""
+const DbName = ""
 
 const SMTP = "smtp.gmail.com"
 const SMTPPort = 587
-const Username = "skybedy@gmail.com"
-const Password = "2d6864ea1184d8ee39a63f01a1e3eb021ec4cba6bcb4ccacfce5d7247ff72615"
+const Username = ""
+const Password = ""
 const MailCharset = "UTF-8"
 const MailFrom = "visalajka@seznam.cz"
 const MailFromName = "timechip.cz"
